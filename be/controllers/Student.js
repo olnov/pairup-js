25f0fb9c7e172be566e9f1b38b92a1660463cc0d
Oleg <o.novikov@ymail.com>
2024-09-16 00:10:55 +0100

    Started student controller

--- be/controllers/Student.js
+++ be/controllers/Student.js
@@ -48,3 +48,30 @@ exports.getStudentById = async (req, res)=> {
         res.status(500).json({ message: 'Error retreiving student.', error:error.message});
     }
 }
+
+// Update student
+exports.updateStudentById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { full_name, email, skill_level, cohort_id } = req.body;
+        const student = await Student.findByPk(id);
+        if (!student) {
+            return res.status(404).json({ message: 'Student not found.'});
+        }
+        if (email) {
+            const studentExists = await Student.findOne({ where: { email }});
+            if (studentExists) {
+                return res.status(409).json({ message: 'Email already registered.'});
+            }
+        }
+        const updatedStudent = await student.update({
+            full_name: full_name || student.full_name,
+            email: email || student.email,
+            skill_level: skill_level || student.skill_level,
+            cohort_id: cohort_id || student.cohort_id       
+        });
+        res.status(200).json({ message: 'Student updated successfully.', updatedStudent });
+    }catch(error){
+        res.status(500).json({ message: 'Error updating student.', error:error.message });
+    }
+}
