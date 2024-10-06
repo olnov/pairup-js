2cdb6fa946119a66cdd65093e87b1f482f2f331e
Oleg <o.novikov@ymail.com>
2024-10-06 21:56:49 +0100

    Added students creation functionality

--- fe/src/pages/Students.tsx
+++ fe/src/pages/Students.tsx
@@ -1,8 +1,9 @@
 import Box from "@mui/material/Box"
 import Sidebar from "../components/Sidebar/Sidebar";
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
 import { useState, useEffect } from "react";
-import { getStudents } from "../services/StudentsService";
+import { createStudent, getStudents } from "../services/StudentsService";
+import { getCohorts } from "../services/CohortsService";
 
 
 const Students = () => {
@@ -11,7 +12,8 @@ const Students = () => {
     const [email, setEmail] = useState<string>("");
     const [skillLevel, setSkillLevel] = useState<string>("");
     const [groups, setGroups] = useState<string[]>([]);
-    const [cohort, setCohort] = useState<string>("");
+    const [cohorts, setCohorts] = useState<any[]>([]);
+    const [cohort_id, setCohort_id] = useState<string>("");
     const [open, setOpen] = useState(false);
     const token = localStorage.getItem("token");
 
@@ -22,7 +24,27 @@ const Students = () => {
 
     useEffect(() => {
         fetchStudents();
+        fetchCohorts();
     }, []);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const fetchCohorts = async () => {
+        const response = await getCohorts(token as string);
+        setCohorts(response);
+    };
+
+    const handleAddStudent = async () => {
+        console.log("Adding student:");
+        console.log(fullName, email, skillLevel, cohort_id);
+        await createStudent(token as string, fullName, email, skillLevel, cohort_id);
+    };
 
     return (
         <Box sx={{ display: 'flex' }}>
@@ -64,11 +86,89 @@ const Students = () => {
                                 <TableCell></TableCell>
                                 <TableCell></TableCell>
                                 <TableCell></TableCell>
-                                <TableCell><Button variant="contained" color="primary">Add Student</Button></TableCell>
+                                <TableCell><Button variant="contained" color="primary" onClick={handleClickOpen}>Add Student</Button></TableCell>
                             </TableRow>
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                        component: 'form',
+                        onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                            event.preventDefault();
+                            const formData = new FormData(event.currentTarget);
+                            const formJson = Object.fromEntries((formData as any).entries());
+                            const fullName = formJson.full_name;
+                            const email = formJson.email;
+                            const skillLevel = formJson.skill_level;
+                            const cohort_id = formJson.cohort_id;
+                            console.log(email);
+                            handleAddStudent();
+                            handleClose();
+                        },
+                    }}
+                >
+                    <DialogTitle>Add new student</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            required
+                            margin="dense"
+                            id="full_name"
+                            name="full_name"
+                            label="Full Name"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            value={fullName}
+                            onChange={(e) => setFullName(e.target.value)}
+                        />
+                        <TextField
+                            required
+                            margin="dense"
+                            id="email"
+                            name="email"
+                            label="Email Address"
+                            type="email"
+                            fullWidth
+                            variant="standard"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <TextField
+                            required
+                            margin="dense"
+                            id="skill_level"
+                            name="skill_level"
+                            label="Skill Level"
+                            type="number"
+                            fullWidth
+                            variant="standard"
+                            value={skillLevel}
+                            onChange={(e) => setSkillLevel(e.target.value)}
+                        />
+                        <FormControl fullWidth variant="standard" margin="dense">
+                        <InputLabel id="cohort-label">Cohort</InputLabel>
+                        <Select
+                            labelId="cohort-label"
+                            id="cohort_id"
+                            value={cohort_id}
+                            label="Specialism"
+                            onChange={(e)=> setCohort_id(e.target.value)}
+                        >
+                            {cohorts.map((cohort) => (
+                                <MenuItem key={cohort.id} value={cohort.id}>{cohort.title}</MenuItem>
+                            ))}
+                        </Select>
+                        </FormControl>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button type="submit">Add</Button>
+                    </DialogActions>
+                </Dialog>
             </Box>
         </Box>
     );
--- fe/src/services/StudentsService.ts
+++ fe/src/services/StudentsService.ts
@@ -12,6 +12,29 @@ export const getStudents = async (token:String)=> {
         const data = await response.json();
         return data;
     }else{
-        throw new Error(`Invalid credentials: ${response.status}`);
+        throw new Error(`Error: ${response.status}`);
     }
 }
+
+export const createStudent = async (token:String, fullName:String, email:String, skillLevel:String, cohortId:String) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            full_name: fullName,
+            email: email,
+            skill_level: skillLevel,
+            cohort_id: cohortId,
+        }),
+    };
+    const response = await fetch(`${BACKEND_URL}/students`, requestOptions);
+    if (response.status === 201) {
+        const data = await response.json();
+        return data;
+    }else{
+        throw new Error(`Error: ${response.status}`);
+    }
+};
